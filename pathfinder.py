960b9bc5f74087a9929d138f52d82f65a700cc35
btaubenheim <bernd.taubenheim@gmail.com>
2014-02-11 19:17:29 +0100

    added indiviedual path definition file pathfinder.py

---
+++ pathfinder.py
@@ -0,0 +1,6 @@
+libPyOrcaROOT="~/software/OrcaROOT/Bindings/libPyOrcaROOT"
+libWaveWaveBase="~/software/TWaveform/lib/libWaveWaveBase"
+OrcaFileFolder="/Users/nedmdaq/Desktop/Data/Data/"
+ROOTFileFolder="/Users/nedmdaq/Desktop/Data/Rootified/"
+TxtFileFolder="/Users/nedmdaq/Desktop/Data/Txtified/"
+
